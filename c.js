b04f64791f07ad26aec8804812e846b9b6f2b53b
Matthew Whitaker <matthew.w3000@gmail.com>
2017-01-30 14:31:09 -0700

    Update c.js

--- c.js
+++ c.js
@@ -4,7 +4,7 @@
 //WITHOUT WRITTEN CONSENT OF MATTHEW WHITAKER
 //AND SUB 6 RESOURCES
 //LICENSE.md may give more details
-function c(n,a) {return (((4*s1(n,a))-(2*s2(n,a))-(s3(n,a))-(s4(n,a)))%1)*16;}
+function c(n,a) {return (((0x4*s1(n,a))-(0x2*s2(n,a))-(s3(n,a))-(s4(n,a)))%0x1)*0xF;}
 function s1(n,a) {
   var k1,k2;
   k1=0;
@@ -16,6 +16,8 @@ function s1(n,a) {
     //TODO start
     k2+=((Math.pow(16,n-k))/((8*k)+1));
   }
+  k1=h(k1);k2=h(k2);
+  return k1+k2;
 }
 function s2(n,a) {
   var k1,k2;
@@ -28,6 +30,8 @@ function s2(n,a) {
     //TODO start
     k2+=((Math.pow(16,n-k))/((8*k)+4));
   }
+  k1=h(k1);k2=h(k2);
+  return k1+k2;
 }
 function s3(n,a) {
   var k1,k2;
@@ -40,6 +44,8 @@ function s3(n,a) {
     //TODO start
     k2+=((Math.pow(16,n-k))/((8*k)+5));
   }
+  k1=h(k1);k2=h(k2);
+  return k1+k2;
 }
 function s4(n,a) {
   var k1,k2;
@@ -52,4 +58,7 @@ function s4(n,a) {
     //TODO start
     k2+=((Math.pow(16,n-k))/((8*k)+6));
   }
+  k1=h(k1);k2=h(k2);
+  return k1+k2;
 }
+function h(nu) {var hS = nu.toString(16); return parseInt(hS, 16);}
