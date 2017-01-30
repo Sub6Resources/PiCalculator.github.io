5810560631b43a38bbf8cf8332cb826c1927313a
Matthew Whitaker <matthew.w3000@gmail.com>
2017-01-30 09:41:02 -0700

    Create c.js

---
+++ c.js
@@ -0,0 +1,21 @@
+//INTENTIONALLY OBFUSCATED CODE
+//ALL RIGHTS RESERVED
+//DO NOT COPY OR REDISTRIBUTE
+//WITHOUT WRITTEN CONSENT OF MATTHEW WHITAKER
+//AND SUB 6 RESOURCES
+//LICENSE.md may give more details
+var B=16;
+function c(n,a) {return (((4*s1(n,a))-(2*s2(n,a))-(s3(n,a))-(s4(n,a)))%1)*B;}
+function s1(n,a) {
+  var k1,k2;
+  //TODO start here.
+}
+function s2(n,a) {
+  var k1,k2;
+}
+function s3(n,a) {
+  var k1,k2;
+}
+function s4(n,a) {
+  var k1,k2;
+}
