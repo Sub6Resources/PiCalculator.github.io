7ef45653a3958176facd72e71c05bec6e655da72
Matthew Whitaker <matthew.w3000@gmail.com>
2017-01-30 11:16:44 -0700

    Update c.js

--- c.js
+++ c.js
@@ -8,9 +8,13 @@ var B=16;
 function c(n,a) {return (((4*s1(n,a))-(2*s2(n,a))-(s3(n,a))-(s4(n,a)))%1)*B;}
 function s1(n,a) {
   var k1,k2;
-  //TODO start here.
-  for(i=0;i<n;i++) {
-    
+  k1=0;
+  k2=0;
+  for(var k=0;k<=n;k++) {
+    k1+=(Math.pow(16,n-k)%((8*k)+1)/(8*k)+1);
+  }
+  for(var k=0;k<=n;k++) {
+    //TODO start
   }
 }
 function s2(n,a) {
