383cd2a41e12e4b9eebf14ad5d3c1457335d2afe
Matthew Whitaker <matthew.w3000@gmail.com>
2017-05-22 09:27:25 -0600

    Update c2.js

--- c2.js
+++ c2.js
@@ -15,25 +15,40 @@ function calculate(NDIGITS) {
         h = 0;
 
     for (; a.length !== LEN; a.push(0));
+    log("A: "+a);
     for (; (b = c -= 14) > 0;) {
+        log("B: "+b);
+        log("C: "+c);
         for (; --b > 0;) {
+            log("B(LOOP): "+b);
             d *= b;
+            log("D: "+d);
             if (h === 0) {
                 d += 2000 * f;
+                log("D(H === 0): "+d);
             } else {
                 d += a[b] * f;
+                log("D(H !== 0): "+d);
             }
             g = b + b - 1;
+            log("G: "+g);
             a[b] = mod(d, g);
+            log("A[B]: "+a[b]);
             d = Math.floor(d / g);
+            log("D(floor): "+d); 
         }
         h = Math.floor(e + d / f);
+        log("H: "+h);
         out += h;
+        log("OUT: "+out);
         h = h.length;
+        log("H.LENGTH: "+h);
         d = e = mod(d, f);
+        log("D(end): "+d);
+        log("E(end): "+e);
     }
+    log("OUT COMING: "+out);
     return out;
 }
-console.log(calculate(100));
 
 function log(me) {document.getElementById("log").innerHTML += '<br />>>> '+me;}
