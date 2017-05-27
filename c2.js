9bda66dcaab3ad73cc52c775a7b708359403fdb4
Matthew Whitaker <matthew.w3000@gmail.com>
2017-05-26 19:02:03 -0600

    Update c2.js

--- c2.js
+++ c2.js
@@ -1,3 +1,4 @@
+//Initialization
 self.importScripts("bignumber.js");
 var NDIGITSi = 100000;
     var LENi = (NDIGITSi / 4 + 1) * 14,
@@ -13,52 +14,44 @@ var NDIGITSi = 100000;
         temph = "";
 var it = 1;
 log("Starting...");
+//Call the function
 calcpi(NDIGITSi, outi, ai, bi, ci, di, ei, fi, gi, hi);
 function calcpi(NDIGITS, out, a, b, c, d, e, f, g, h) {
+    log("Calculating "+NDIGITS+" digits of pi...<br />");
     var LEN = (NDIGITS / 4 + 1) * 14;
     c = LEN;
     f = 10000;
-    log("Calculating "+NDIGITS+" digits of pi...<br />");
     for (; a.length !== LEN; a.push(0));
-    //console.log("A: "+a);
     for (; (b = c -= 14) > 0;) {
-        //console.log("B: "+b);
-        //console.log("C: "+c);
         for (; --b > 0;) {
-            //console.log("B(LOOP): "+b);
             d *= b;
-            //console.log("D: "+d);
             if (h === 0) {
                 d += 2000 * f;
-                //console.log("D(H === 0): "+d);
             } else {
-                d += a[b] * f;
-                //console.log("D(H !== 0): "+d);
+                d += a[b] * f;]
             }
-            g = b + b - 1;
-            //console.log("G: "+g);
-            a[b] = mod(d, g);
-            //console.log("A[B]: "+a[b]);
-            d = Math.floor(d / g);
-            //console.log("D(floor): "+d); 
+            g = b + b - 1;]
+            a[b] = mod(d, g);]
+            d = Math.floor(d / g);]
         }
         h = Math.floor(e + d / f);
-        //console.log("H: "+h);
         temph = h+"";
-        if(temph.length == 1) {
+        var temphLength = temph.length;
+        if(temphLength == 1) {
           temph = "000"+temph;
         }
-        else if(temph.length == 2) {
+        else if(temphLength == 2) {
           temph = "00"+temph;
         }
-        else if(temph.length == 3) {
+        else if(temphLength == 3) {
           temph = "0"+temph;
         }
-        if(temph.length == 5) {
+        else if(temphLength == 5) {
             //Probably a 10000, add one to digit before, and put last four digits into this set.
             var lastNumber = out.slice(-1);
             out = out.slice(0, -1);
             var numToRepl = (((lastNumber*1) + (temph.charAt(0)*1)))+"";
+            //Remove in production
             replacePrev(numToRepl); //Send message to page telling to replace number before.
             out += numToRepl;
             temph = temph.substring(1, temph.length);
@@ -70,17 +63,10 @@ function calcpi(NDIGITS, out, a, b, c, d, e, f, g, h) {
             log(it+"00 digits: ");
             it++;
         }
-        //console.log("OUT: "+out);
         h = h.length;
-        //console.log("H.LENGTH: "+h);
         d = e = mod(d, f);
-        //console.log("D(end): "+d);
-        //console.log("E(end): "+e);
     }
-    //console.log("OUT COMING: "+out);
-    log("DONE");
-    //calcpi(NDIGITS+1000, out, a, b, c,d,e,f,g,h);
-//log("RESULT: "+out);
+    log("DONE! Calculated "+out.length+" digits of pi!);
 }
 function add(me) {postMessage(me);}
 function replacePrev(newNum) {postMessage("REPL:"+newNum);}
